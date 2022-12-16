bd512cce04077da828a1726d83031c8a1efae4e9
Jorun <jorun.kodehode@gmail.com>
2022-12-16 10:05:20 +0100

    adding inside styling +

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,12 +9,12 @@ function Navbar() {
         <NavLink title="Scream" text="scream" />
         <NavLink title="IceCream" text="Ice cream" />
       </nav>
-      <ComponentWithChildren>
+      <ComponentChildren>
         <h3>Child 1</h3>
         <h3>Child 2</h3>
         <h3>Child 3</h3>
         <img src="https://images.unsplash.com/photo-1661956602926-db6b25f75947?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=698&q=80" />
-      </ComponentWithChildren>
+      </ComponentChildren>
     </div>
   );
 }
@@ -29,7 +29,7 @@ function NavLink(props) {
     </li>
   );
 }
-function ComponentWithChildren(props) {
+function ComponentChildren(props) {
   console.log(props);
   return <div>{props.children}</div>;
 }
